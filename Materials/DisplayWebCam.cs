bf2e0cfe3bfa4358497d0a8107e1e85b9bef4789
LukeKendall2 <lkendall@villanova.edu>
2016-11-01 21:48:40 -0400

    Add files via upload

---
+++ Materials/DisplayWebCam.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayWebCam : MonoBehaviour
+{
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        // for debugging purposes, prints available devices to the console
+        for (int i = 0; i < devices.Length; i++)
+        {
+            print("Webcam available: " + devices[i].name);
+        }
+
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+
+        // assuming the first available WebCam is desired
+        WebCamTexture tex = new WebCamTexture(devices[0].name);
+        rend.material.mainTexture = tex;
+        tex.Play();
+    }
+}
+/// <summary>
+/// Replace the given texture (usually our own) with a texture to be rendered by the given camera.
+/// You may supply a renderTexture to use, or one will be ceated.
+///
+/// by Matt "Trip" Maker, Monstrous Company :: http://monstro.us
+///
+/// </summary>
